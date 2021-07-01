58e4c864699ea49941f48b3a6696b0aaa1ef7d78
Ben T <BJTomhave@hotmail.com>
2021-07-01 08:46:08 -0500

    Changed format for travelTimes

--- R/travelTimes.R
+++ R/travelTimes.R
@@ -10,51 +10,59 @@
 #' @return summary of travel times in specified time range
 #' @export
 #' 
-generateTravelTimeSummary <- function(gtfs,beginStopID, endStopID,
+generateTravelTimeSummary <- function(gtfs, beginStopID, endStopID,
                                       routeID, startHHMMSSRange, endHHMMSSRange) {
  
+  # Define piping function
+  `%>%` <- magrittr::`%>%`
+  
   ## Load relevant gtfs data
   stop_times <- gtfs$stop_times
   trips <- gtfs$trips
   
   # Convert to datatable
-  setDT(trips)
-  setDT(stop_times)
+  data.table::setDT(trips)
+  data.table::setDT(stop_times)
   
   # Join trips and stop times and only keep relevant stops
-  sch <- trips[stop_times, on = 'trip_id']%>%filter(as.character(stop_id) %in% c(beginStopID, endStopID) & as.character(route_id) == as.character(routeID))
+  sch <- trips[stop_times, on = 'trip_id']%>%filter(as.character(stop_id) %in% c(as.character(beginStopID), as.character(endStopID)) & as.character(route_id) == as.character(routeID))
   
   # Convert time format to integer seconds
   sch$arrival_time <- gtfsFunctions::as.TransitTime(sch$arrival_time)
   sch$departure_time <- gtfsFunctions::as.TransitTime(sch$departure_time)
   
   # Order by arrival_time from morning to evening
-  sch <- setorder(sch, arrival_time)
+  sch <- data.table::setorder(sch, arrival_time)
   
   
   # Get unique trips in time frame 
-  sch <- sch%>%filter(arrival_time >= as.integer(gtfsFunctions::as.TransitTime(as.character(startHHMMSSRange))) & arrival_time <= as.integer(gtfsFunctions::as.TransitTime(as.character(endHHMMSSRange))))
+  sch <- sch %>% dplyr::filter(arrival_time >= as.integer(gtfsFunctions::as.TransitTime(as.character(startHHMMSSRange))) & arrival_time <= as.integer(gtfsFunctions::as.TransitTime(as.character(endHHMMSSRange))))
   tripList <- unique(sch$trip_id)
   
   # Calculate travel times only if, for the given trip, both selected stops are served
   travelTimeList <- c()
   for (trip in tripList){
-    test <- sch%>%filter(trip_id == trip)
-    if (nrow(test) > 1){
-      tt <- round(as.integer(test[-1,arrival_time] - test[-2,arrival_time])/60)
-      travelTimeList <- c(travelTimeList, tt)
+    singleTripData <- sch%>% dplyr::filter(trip_id == trip)
+    if (nrow(singleTripData) > 1){
+      # Check to make sure order of stops is correct
+      if (as.numeric(singleTripData[ as.character(stop_id) == as.character(beginStopID), ]$stop_sequence) <= singleTripData[ as.character(stop_id) == as.character(endStopID), ]$stop_sequence){
+        tt <- round(as.integer(singleTripData[-1,arrival_time] - singleTripData[-2,arrival_time])/60)
+        travelTimeList <- c(travelTimeList, tt)
+      }else{
+        # Custom error if if statement is false
+        stop('Error: Stop sequence of endStopID is greater than beginStopID. Switch their order.')
+      }
     }
   }
-  # # Get avg travel time
-  # print(mean(travelTimeList))
-  # 
-  # Get summary of travel times
-  summary(travelTimeList)
-  
-  
- 
-}
 
+  # Get summary of travel times
+  if (length(travelTimeList) > 0){
+    summary(travelTimeList)
+  }else{
+    #Custom error statement
+    stop('Error: Data does not exist for the selected combination of stop ids time of day and route. Please change inputs.')
+  }
+}
 
 
 #' Calculate travel time median between two stop_ids on a given route
