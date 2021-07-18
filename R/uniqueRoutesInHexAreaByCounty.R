a497a989d7534c5cb9ebee3ebfad20c1a8fbf7af
Ben <bentomhave@BenTomhaves-MacBook-Pro.local>
2021-07-18 10:35:26 -0500

    Update HexTesselation to not require acs

--- R/uniqueRoutesInHexAreaByCounty.R
+++ R/uniqueRoutesInHexAreaByCounty.R
@@ -1,43 +1,40 @@
 #' Return spatial polygon of hexagonal features with a count of the number of unique routes with a stop inside the hex area's contained in the specified counties
 #'
-#' @param gtfs object from gtfsfunctions::formatGTFSObject()
-#' @param stateAbbrev Character string or vector of character strings of state abbreviations or State FIPS codes. 
-#' @param counties Character string or vector of character strings of desired counties in state or County FIPS codes.
+#' @param gtfsObj object from gtfsfunctions::formatGTFSObject()
 #' @param tidyCensusAPIKey String for tidy_census::get_acs() api key
 #' @param hexSize number specifying hex size (smaller value creates more smaller sized hexes but takes longer to load). Default = 0.01
 #'
 #' @return returns an sf data frame with 4 columns (hex_id, routes, uniqueRouteCount, geometry), where routes contains a character vector of routes in the hex and uniqueRouteCount counts the number of routes in each hex
 #' @export
 #' 
-uniqueRoutesInHexTessalation <- function(gtfs, stateAbbrev = NULL, counties = NULL, tidyCensusAPIKey = NULL, hexSize = 0.01) {
+uniqueRoutesInHexTessalation <- function(gtfsObj, tidyCensusAPIKey = NULL, hexSize = 0.01) {
   
   # Define piping function
   `%>%` <- magrittr::`%>%`
   
-  simpleRoutesAtStops <- gtfsFunctions::simpleRoutesAtStops(gtfs)%>%
+  simpleRoutesAtStops <- gtfsFunctions::simpleRoutesAtStops(gtfsObj)%>%
                               sf::st_as_sf(coords = c("stop_lon", "stop_lat"), crs = 4326)
   
+  # Create Rectangular Polygon Containing All Points For Hex Filling
+  boundingBoxCoords <- rbind(c(min(gtfsObj$stops$stop_lon), min(gtfsObj$stops$stop_lat)),
+                             c(min(gtfsObj$stops$stop_lon), max(gtfsObj$stops$stop_lat)),
+                             c(max(gtfsObj$stops$stop_lon), max(gtfsObj$stops$stop_lat)),
+                             c(max(gtfsObj$stops$stop_lon), min(gtfsObj$stops$stop_lat)),
+                             c(min(gtfsObj$stops$stop_lon), min(gtfsObj$stops$stop_lat)))
   
-  countyBoundaries <- tidycensus::get_acs(geography = "county",
-                                    key = tidyCensusAPIKey,
-                                    variables = "B01003_001",
-                                    state = stateAbbrev,
-                                    county = counties,
-                                    year = 2016,
-                                    survey = "acs5",
-                                    geometry = T,
-                                    output = "wide") %>% sf::st_transform(crs = 4326)
+  gtfsBBoxPolygon <-sf::st_polygon(list(boundingBoxCoords))%>%sf::st_sfc(crs = 4326)
+  
   
   # Get Hex Tessalation
   size <- hexSize
   
-  hex_points <- sp::spsample(sf::as_Spatial(countyBoundaries), type = "hexagonal", cellsize = size)
+  hex_points <- sp::spsample(sf::as_Spatial(gtfsBBoxPolygon), type = "hexagonal", cellsize = size)
   hex_grid <- sf::st_as_sf(sp::HexPoints2SpatialPolygons(hex_points, dx = size))
   hex_grid$hex_id <- seq(1:nrow(hex_grid))
   
   
   # Get Routes at Stops
-  routesAtStops <- gtfsFunctions::namedList_RoutesAtStops(gtfs)
+  routesAtStops <- gtfsFunctions::namedList_RoutesAtStops(gtfsObj)
   
   # Get List of Routes at Stop in Better Format
   simpleRoutesAtStops$RouteList <- routesAtStops[simpleRoutesAtStops$stop_id]
@@ -57,3 +54,5 @@ uniqueRoutesInHexTessalation <- function(gtfs, stateAbbrev = NULL, counties = NU
   
   return(spatial_hex2Routes)
 }
+
+
