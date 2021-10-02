58063b08d75e1b87aab496a7e5336c69cb3ed62d
Ben T <BJTomhave@hotmail.com>
2021-10-01 21:09:14 -0500

    Add headsign to frequency calculation

--- R/adapted_tidytransit.R
+++ R/adapted_tidytransit.R
@@ -2,6 +2,7 @@
 #'
 #' @param gtfs_obj object from tidytransit read_gtfs()
 #' @param by_directionId (optional) TRUE/FALSE indicating if segmenting route frequencies by direction_id (default TRUE)
+#' @param by_headsign (optional) TRUE/FALSE indicating if segmenting frequencies trip_headsgign overall.
 #' @param by_TOD (optional) TRUE/FALSE indicating if segmenting frequencies by time of day periods or overall. If by_TOD == T don't provide start and end HHMMSS
 #' @param tod_earlyStart   (optional) If by_TOD = False, specify an integer (seconds past midnight) for tod Early period start (default 14400 (4AM))
 #' @param tod_AMPeakStart  (optional) If by_TOD = False, specify an integer (seconds past midnight) for tod AM Peak period start (default 21600 (6AM))
@@ -20,6 +21,7 @@
 #' 
 get_stop_frequency <- function(gtfs_obj,
                                by_directionId = T,
+                               by_headsign = T,
                                by_TOD = T,
                                tod_earlyStart   = 14400,
                                tod_AMPeakStart  = 21600,
@@ -41,15 +43,30 @@ get_stop_frequency <- function(gtfs_obj,
   # Convert data.frame to data table
   data.table::setDT(gtfs_obj$stop_times)
   data.table::setDT(gtfs_obj$trips)
+  
+  # Check if trip_headsign exists. If not create headsign as last stop sequence stop name per trip
+  if(!exists("trip_headsign", where = gtfs_obj$trips)) {
+    
+    # get the last id of the trip's stop sequence
+    trip_headsigns <- gtfs_obj$stop_times %>% 
+      group_by(trip_id) %>% 
+      summarise(stop_id = stop_id[which.max(stop_sequence)]) %>% 
+      left_join(gtfs_obj$stops, by="stop_id") %>% select(trip_id, trip_headsign = stop_name)
+    
+    # assign the headsign to the gtfs object 
+    gtfs_obj$trips <- left_join(gtfs_obj$trips, trip_headsigns, by = "trip_id")
+  }
 
-
+  # Trip2Headsign Lookup Table
+  trip2Headsign <- gtfs_obj$trips[, .(trip_id, trip_headsign)]
+  
   # If no specified service_ids then create frequencies for all service ids
   if(is.null(service_ids)) {
     # Get number of departures per trip
     dep_per_trip <- gtfs_obj$stop_times[, .(n_deps = data.table::uniqueN(trip_id)), by =.(trip_id)]
     
     # Get number of departures per service_id
-    dep_per_service_id <- gtfs_obj$trips[dep_per_trip, on = 'trip_id'][, .(n_deps = sum(n_deps)), by = "service_id"]
+    dep_per_service_id <- gtfs_obj$trips[dep_per_trip, on = 'trip_id'][, .(n_deps = sum(n_deps)), by = .(service_id)]
     data.table::setorder(dep_per_service_id, -n_deps) # Sort desc by n_deps
     
     
@@ -97,12 +114,24 @@ get_stop_frequency <- function(gtfs_obj,
   # Remove NAs that popup on join
   stop_times <- na.omit(unique(stop_times[trips[, c("trip_id", "route_id", "direction_id", "service_id")], on = .(trip_id)]))
 
+  # Join Headsign to stop_times
+  stop_times <- stop_times[trip2Headsign, on = .(trip_id)]
   # find number of departure per stop_id, route_id, service_id
   # If aggregated by route_id
   if(by_directionId == T) {
-    freq <- stop_times[, .(n_departures = .N, periodMinutes = periodMinutes, timeSpan = timeSpan),  by = .(stop_id, route_id, direction_id, service_id, period)]
-  } else {
-    freq <- stop_times[, .(n_departures = .N, periodMinutes = periodMinutes, timeSpan = timeSpan),  by = .(stop_id, route_id, service_id, period)]
+    # Check if Also Aggregating By Headsign
+    if(by_headsign == T) { #Group by direction and headsign
+      freq <- stop_times[, .(n_departures = .N, periodMinutes = periodMinutes, timeSpan = timeSpan),  by = .(stop_id, route_id, direction_id, service_id, period, trip_headsign)]
+    }else{ # Group by direction but not headsign
+      freq <- stop_times[, .(n_departures = .N, periodMinutes = periodMinutes, timeSpan = timeSpan, trip_headsign = "Not Generated"),  by = .(stop_id, route_id, direction_id, service_id, period)]
+    }
+  } else { # Group by headsign but not direction
+    if(by_headsign == T) {
+      freq <- stop_times[, .(n_departures = .N, periodMinutes = periodMinutes, timeSpan = timeSpan),  by = .(stop_id, route_id, service_id, period, trip_headsign)]
+    }else{ # Group by neither direction nor headsign
+      freq <- stop_times[, .(n_departures = .N, periodMinutes = periodMinutes, timeSpan = timeSpan, trip_headsign = "Not Generated"),  by = .(stop_id, route_id, service_id, period)]
+      
+    }
   }
   
 
@@ -118,6 +147,7 @@ get_stop_frequency <- function(gtfs_obj,
 #'
 #' @param gtfs_obj object from tidytransit read_gtfs()
 #' @param by_directionId (optional) TRUE/FALSE indicating if segmenting route frequencies by direction_id (default TRUE)
+#' @param by_headsign (optional) TRUE/FALSE indicating if segmenting frequencies trip_headsgign overall.
 #' @param by_TOD (optional) TRUE/FALSE indicating if segmenting frequencies by time of day periods or overall. If by_TOD == T don't provide start and end HHMMSS
 #' @param tod_earlyStart   (optional) If by_TOD = False, specify an integer (seconds past midnight) for tod Early period start (default 14400 (4AM))
 #' @param tod_AMPeakStart  (optional) If by_TOD = False, specify an integer (seconds past midnight) for tod AM Peak period start (default 21600 (6AM))
@@ -136,6 +166,7 @@ get_stop_frequency <- function(gtfs_obj,
 #' 
 get_route_frequency <- function(gtfs_obj,
                                 by_directionId = T,
+                                by_headsign = T,
                                 by_TOD = T,
                                 tod_earlyStart   = 14400,
                                 tod_AMPeakStart  = 21600,
@@ -152,6 +183,7 @@ get_route_frequency <- function(gtfs_obj,
   # Get Frequency of all routes at all stops they servce
   stops_frequency <- get_stop_frequency(gtfs_obj,
                                         by_directionId,
+                                        by_headsign,
                                         by_TOD,
                                         tod_earlyStart,
                                         tod_AMPeakStart,
@@ -171,7 +203,7 @@ get_route_frequency <- function(gtfs_obj,
     if (by_TOD == T){
       if (by_directionId == T){
         routes_frequency <- stops_frequency %>%
-          dplyr::group_by(route_id, direction_id, period, timeSpan) %>%
+          dplyr::group_by(route_id, direction_id, trip_headsign, period, timeSpan) %>%
           dplyr::summarise(total_departures = sum(n_departures),
                            median_headways = 
                            as.integer(round(median(headwayMinutes),0)),
@@ -182,7 +214,7 @@ get_route_frequency <- function(gtfs_obj,
                            stop_count = dplyr::n())
         }else{
           routes_frequency <- stops_frequency %>%
-          dplyr::group_by(route_id, period, timeSpan) %>%
+          dplyr::group_by(route_id, trip_headsign, period, timeSpan) %>%
           dplyr::summarise(total_departures = sum(n_departures),
                            median_headways = 
                            as.integer(round(median(headwayMinutes),0)),
@@ -196,7 +228,7 @@ get_route_frequency <- function(gtfs_obj,
       if (by_directionId == T){
         routes_frequency <- stops_frequency %>%
           distinct() %>%
-          dplyr::group_by(route_id, direction_id, timeSpan) %>%
+          dplyr::group_by(route_id, direction_id,trip_headsign, timeSpan) %>%
           dplyr::summarise(total_departures = sum(n_departures),
                            median_headways = 
                              as.integer(round(median(headwayMinutes),0)),
@@ -208,7 +240,7 @@ get_route_frequency <- function(gtfs_obj,
       }else{
         routes_frequency <- stops_frequency %>%
           distinct() %>%
-          dplyr::group_by(route_id, timeSpan) %>%
+          dplyr::group_by(route_id, trip_headsign, timeSpan) %>%
           dplyr::summarise(total_departures = sum(n_departures),
                            median_headways = 
                            as.integer(round(median(headwayMinutes),0)),
@@ -218,6 +250,11 @@ get_route_frequency <- function(gtfs_obj,
                            round(sd(headwayMinutes),2),
                            stop_count = dplyr::n())
       }
+    }
+    # If not aggregating by headsign drop that column and get distinct as it has placeholder 'Not Generated' as created in stops_frequency
+    data.table::setDT(routes_frequency) # Make sure output is data.table
+    if (by_headsign == F){
+      routes_frequency <- routes_frequency[, trip_headsign := NULL] %>% dplyr::distinct()
     }
   }else {
     warning("Failed to calculate frequency, try passing a service_id from calendar_df.")
@@ -244,20 +281,21 @@ get_route_frequency <- function(gtfs_obj,
 # gtfs_obj <- gtfsFunctions::formatGTFSObject(gtfsPath_Jul)
 # # aweseom <- tidytransit::read_gtfs(gtfsPath_Jul2)
 # # test <- gtfsFunctions::routeIDAtStops(gtfs3)
-# #
-# library(dplyr)
+# 
 # ptm <- proc.time()
 # # 1 Second
 # 
-# stops <- get_stop_frequency(gtfs_obj,
-#                                            by_directionId = F,
+# stops <-get_stop_frequency(gtfs_obj,
+#                                             by_directionId = F,
+#                                             by_headsign = T,
 #                                             by_TOD = T,
 #                                             service_ids = NULL)
 # 
 # ptm <- proc.time()
-# route_frequency2 <-  get_route_frequency(gtfs_obj,
-#                                                         by_directionId = F,
-#                                                         by_TOD = F,
+# route_frequency2 <-   get_route_frequency(gtfs_obj,
+#                                                         by_directionId = T,
+#                                                         by_headsign = T,
+#                                                         by_TOD = T,
 #                                                         # tod_earlyStart   = 14400,
 #                                                         # tod_AMPeakStart  = 22000,
 #                                                         # tod_MiddayStart  = 32400,
